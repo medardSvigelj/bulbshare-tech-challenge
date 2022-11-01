e33486e31311228ffeb0dc7e40c038214aa1e33b
jb <jb.rougeot@lightdash.com>
2022-11-01 10:39:25 +0000

    remove changes in server

--- server/index.js
+++ server/index.js
@@ -1,18 +1,12 @@
 require('dotenv').config();
 const express = require('express')
 const path = require('path');
-const feed = require ("./data/feed.json");
 const cors = require("cors");
 
 
 const app = express()
 app.use(cors());
 app.use(express.static(path.join(__dirname,'assets')));
-
-app.get("/feed",(req, res) => {
-  res.send(feed);
-})
-
 
 app.listen(4000, function (err) {
   if (err) return err
